8b49a067e8dd54e7d3fc833f6132fd9e4fa21102
Raman Preet Singh <ramanpre0810@gmail.com>
2020-12-28 23:01:40 +0530

    Added project

---
+++ js/finder.js
@@ -0,0 +1,48 @@
+const api_URL = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+const tilesURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+// Setting Icon for the map
+var ISSicon = L.icon({
+	iconUrl: './icons/iss200.png',
+	iconSize: [50, 32],
+	iconAnchor: [25, 16]
+});
+
+// Setting Map canves
+const mymap = L.map('ISSmap').setView([0, 0], 3);
+
+// Adding tiles to map
+L.tileLayer(tilesURL, { attribution }).addTo(mymap);
+
+// Setting ISS marker
+var marker = L.marker([0, 0], { icon: ISSicon }).addTo(mymap);
+
+// Function to set data in HTML
+function setData(json) {
+	let lat = json.latitude;
+	let long = json.longitude;
+	let alt = json.altitude;
+	let vel = json.velocity;
+
+	mymap.setView([lat, long], mymap.getZoom());
+	marker.setLatLng([lat, long]);
+
+	document.getElementById('lat').textContent = Number.parseFloat(lat).toFixed(2);
+	document.getElementById('long').textContent = Number.parseFloat(long).toFixed(2);
+	document.getElementById('alt').textContent = Number.parseFloat(alt).toFixed(2) + ' km';
+	document.getElementById('vel').textContent = Number.parseFloat(vel).toFixed(2) + ' km/h';
+}
+
+// Function to get the data from api.
+function getISS() {
+	fetch(api_URL)
+		.then(responce => responce.json())
+		.then(json => setData(json));
+
+}
+getISS();
+
+setInterval(getISS, 1000);
